5053183302030d8df10db99b809582dfd8953eaa
Kaan BİBER <kaan.biber53@gmail.com>
2026-04-23 15:19:58 +0300

    Delete Scripts directory

--- Scripts/IInteractable.cs
+++
@@ -1,8 +0,0 @@
-using UnityEngine;
-
-public interface IInteractable
-{
-
-    public void Interact(bool isPositiveInteraction, int creditChangeAmount); 
-
-}
--- Scripts/ScriptableObjects/TaskDatabaseSO.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "TaskDatabaseSO", menuName = "ScriptableObjects/TaskDatabaseSO")]
-public class TaskDatabaseSO : ScriptableObject
-{
-    [Header("All Tasks Pool")]
-    public List<TaskSO> allTasks = new List<TaskSO>();
-}
